5cf4f6054ce21b0050ca57ea08904f2c2d39eb62
kdl-00 <kloresco_21ur0185@Psu.edu.ph>
2025-09-10 16:04:41 +0800

    Move project files into HW2 folder

---
+++ HW2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter row: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter col: ");
+            int col = int.Parse(Console.ReadLine());
+
+            List<List<double>> darray = new List<List<double>>();
+
+            for (int x = 0; x < row; x++)
+            {
+                Console.WriteLine($"Row {x + 1}");
+                List<double> rows = new List<double>();
+
+                for (int y = 0; y < col; y++)
+                {
+                    Console.Write($"Enter number{y + 1}: ");
+                    double num = double.Parse(Console.ReadLine());
+                    rows.Add(num);
+                }
+                darray.Add(rows);
+            }
+
+            Console.WriteLine("\nThe numbers are:");
+            double sum = 0;
+            int count = 0;
+
+            foreach (var rows in darray)
+            {
+                foreach (var num in rows)
+                {
+                    Console.Write($"{num:0.0} ");
+                    sum += num;
+                    count++;
+                }
+                Console.WriteLine();
+            }
+
+            double average = sum / count;
+
+            Console.WriteLine($"\nSum: {sum}; Average: {average}");
+        }
+    }
+}
